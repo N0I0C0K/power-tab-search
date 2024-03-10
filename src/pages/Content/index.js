25be60edd54c6e70e2ae25a59f00f634aa64d8bf
N0I0C0K <nick131410@aliyun.com>
2024-03-10 22:55:23 +0800

    feat: 测试可行性

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,11 @@
-import { printLine } from './modules/print';
+function test() {
+  //   const head = document.getElementsByClassName('devsite-page-title')[0];
+  //   const newP = document.createElement('h1');
+  //   newP.appendChild(document.createTextNode('this is extension created!'));
+  //   head.parentElement.insertAdjacentElement('afterbegin', newP);
+  const head = document.querySelector('h1')
+  head.insertAdjacentText('afterend', 'extension word!')
+  console.log('extsion work too!!!')
+}
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+test()
