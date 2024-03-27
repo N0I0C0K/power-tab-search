80810dffc6ecd5ebb72835e4e995a6bf783b9721
N0I0C0K <nick131410@aliyun.com>
2024-03-28 00:15:42 +0800

    feat: update ui

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -7,13 +7,28 @@ import withErrorBoundary from '@src/shared/hoc/withErrorBoundary'
 import { SearchResult } from '@src/types'
 import { Button } from '@src/components/ui/button'
 import { Input } from '@src/components/ui/input'
+import { Skeleton } from '@src/components/ui/skeleton'
 import { sendMessage, sendMessageToTab } from '@shared/helper/message'
 
 const ResultItem: React.FC<SearchResult> = res => {
+  const [loaded, setLoaded] = useState<'loading' | 'success' | 'failed'>('loading')
   return (
-    <div className="flex gap-2 p-2">
-      <img src={res.icon} alt="img" />
-      <div>
+    <div className="flex gap-2 p-2 shadow-sm border rounded-md duration-200 hover:shadow-lg hover:translate-x-1">
+      {loaded === 'loading' || loaded === 'success' ? (
+        <img
+          className="self-center"
+          src={res.icon}
+          alt="img"
+          onLoad={() => {
+            setLoaded('success')
+          }}
+          onError={() => {
+            setLoaded('failed')
+          }}
+        />
+      ) : null}
+      {loaded === 'loading' ? <Skeleton className="h-10 w-10" /> : null}
+      <div className="flex-1">
         <h3>{res.title}</h3>
         <p>{res.subTitle}</p>
       </div>
@@ -39,7 +54,15 @@ const ResultItem: React.FC<SearchResult> = res => {
 
 const Popup = () => {
   const [text, setText] = useState('')
-  const [result, setResult] = useState<SearchResult[]>()
+  const [result, setResult] = useState<SearchResult[]>([
+    {
+      nodeId: '1',
+      score: 1,
+      subTitle: 'test....',
+      tabId: 11111,
+      title: 'Test!!!',
+    },
+  ])
   const [segment] = useState(new Intl.Segmenter('zh', { granularity: 'word' }))
   const splitText = useMemo(() => {
     return (text: string): string[] => {
@@ -47,10 +70,12 @@ const Popup = () => {
     }
   }, [segment])
   return (
-    <div className="App p-4">
+    <div className="App p-4 flex-col gap-2">
       <div className="flex gap-4">
         <Input
           value={text}
+          autoFocus={true}
+          placeholder="text to search"
           onChange={ev => {
             setText(ev.target.value)
           }}
@@ -67,7 +92,7 @@ const Popup = () => {
           Search
         </Button>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 pt-2 pb-2">
         {result?.map(v => {
           return <ResultItem key={v.nodeId} {...v} />
         })}
